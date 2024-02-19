d9bbf91799faf02a73e3e7ff0fb913a1e57b4e6c
mamarif1555 <mamarif1555@gmail.com>
2024-02-19 11:30:02 +0600

    js start

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+// function hndleSelect(){
+//   console.log("Start...")
+// }
+
+
+const allBtn = document.getElementsByClassName("add-btn");
+
+
+let count = 0;
+
+for(const btn of allBtn){
+  btn.addEventListener("click", function (e){
+    count = count + 1;
+    setInerText("set-count",count)
+  })
+}
+
+function setInerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
